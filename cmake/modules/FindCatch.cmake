6ccbdf12190b392b60b4d10258d678178ce8f419
Ethan Chan <metakirby5@gmail.com>
2017-04-13 23:09:52 -0700

    fix catch

--- cmake/modules/FindCatch.cmake
+++ cmake/modules/FindCatch.cmake
@@ -1,13 +1,16 @@
 include(ExternalProject)
 externalproject_add(catch
+   CONFIGURE_COMMAND ""
+   BUILD_COMMAND ""
+   INSTALL_COMMAND ""
    GIT_REPOSITORY https://github.com/philsquared/Catch.git)
 
-# Expose required variable (CATCH_INCLUDE_DIR) to parent scope
+# Expose required variable (Catch_INCLUDE_DIR) to parent scope
 externalproject_get_property(catch SOURCE_DIR)
-set(CATCH_INCLUDE_DIRS ${SOURCE_DIR}/single_include
+set(Catch_INCLUDE_DIRS ${SOURCE_DIR}/single_include
    CACHE INTERNAL "Path to include folder for Catch")
 
 # find_package boilerplate
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Catch DEFAULT_MSG CATCH_INCLUDE_DIRS)
-mark_as_advanced(CATCH_INCLUDE_DIRS)
+find_package_handle_standard_args(Catch DEFAULT_MSG Catch_INCLUDE_DIRS)
+mark_as_advanced(Catch_INCLUDE_DIRS)
