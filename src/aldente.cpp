a6fa29ea7d698b8fe0415576208324402c15a2e0
Ethan Chan <metakirby5@gmail.com>
2017-04-14 21:09:13 -0700

    timer2 and c++11

--- src/aldente.cpp
+++ src/aldente.cpp
@@ -7,7 +7,9 @@ void aldente() {
   namespace asio = boost::asio;
 
   asio::io_service io;
-  asio::deadline_timer t(io, boost::posix_time::seconds(1));
-  t.wait();
-  std::cout << HELLO_WORLD << std::endl;
+  asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.async_wait([](const boost::system::error_code &) {
+      std::cout << "waited" << std::endl;
+      });
+  io.run();
 }
