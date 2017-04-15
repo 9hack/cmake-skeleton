b6b7ac31b4e1fed8eea66b87a3c7c53577f66b51
Ethan Chan <metakirby5@gmail.com>
2017-04-15 02:27:24 -0700

    timer.5 kind of

--- src/aldente.cpp
+++ src/aldente.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
 #include "aldente.hpp"
 
 void aldente() {
@@ -9,20 +11,33 @@ void aldente() {
   namespace bsys = boost::system;
 
   asio::io_service io;
-  asio::deadline_timer t(io, pst::seconds(1));
+  asio::io_service::strand s(io);
+  asio::deadline_timer t1(io, pst::seconds(1));
+  asio::deadline_timer t2(io, pst::seconds(1));
 
   int count = 0;
-  std::function<void (const bsys::error_code &)> counter =
+  std::function<void (const bsys::error_code &)> c1 = s.wrap(
     [&](const bsys::error_code &) {
       if (count < 5) {
         ++count;
-        std::cout << "Counted " << count << " times." << std::endl;
-        t.expires_at(t.expires_at() + pst::seconds(1));
-        t.async_wait(counter);
+        std::cout << "c1: Counted " << count << " times." << std::endl;
+        t1.expires_at(t1.expires_at() + pst::seconds(1));
+        t1.async_wait(c1);
       }
-    };
-  t.async_wait(counter);
+    });
+  std::function<void (const bsys::error_code &)> c2 = s.wrap(
+    [&](const bsys::error_code &) {
+      if (count < 5) {
+        ++count;
+        std::cout << "c2: Counted " << count << " times." << std::endl;
+        t2.expires_at(t2.expires_at() + pst::seconds(1));
+        t2.async_wait(c2);
+      }
+    });
+  t1.async_wait(c1);
+  t2.async_wait(c2);
 
-  io.run();
+  boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
+  t.join();
   std::cout << "Final count: " << count << "." << std::endl;
 }
