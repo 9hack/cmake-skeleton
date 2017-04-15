be5aade0e3ffca3677671563ffb190b8bac25160
Ethan Chan <metakirby5@gmail.com>
2017-04-14 22:25:59 -0700

    timer.3

--- src/aldente.cpp
+++ src/aldente.cpp
@@ -5,11 +5,24 @@
 
 void aldente() {
   namespace asio = boost::asio;
+  namespace pst = boost::posix_time;
+  namespace bsys = boost::system;
 
   asio::io_service io;
-  asio::deadline_timer t(io, boost::posix_time::seconds(5));
-  t.async_wait([](const boost::system::error_code &) {
-      std::cout << "waited" << std::endl;
-      });
+  asio::deadline_timer t(io, pst::seconds(1));
+
+  int count = 0;
+  std::function<void (const bsys::error_code)> counter =
+    [&](const bsys::error_code &) {
+      if (count < 5) {
+        ++count;
+        std::cout << "Counted " << count << " times." << std::endl;
+        t.expires_at(t.expires_at() + pst::seconds(1));
+        t.async_wait(counter);
+      }
+    };
+  t.async_wait(counter);
+
   io.run();
+  std::cout << "Final count: " << count << "." << std::endl;
 }
