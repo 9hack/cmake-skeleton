2a7230e742814a5f374b5bc78fff0ee2f7c028c8
Ethan Chan <metakirby5@gmail.com>
2017-04-15 17:39:11 -0700

    event manager

--- src/aldente.cpp
+++ src/aldente.cpp
@@ -1,43 +1,56 @@
 #include <iostream>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/thread/thread.hpp>
-#include "aldente.hpp"
+#include <list>
+
+using namespace std;
+
+template <class...Args>
+class EventManager {
+  typedef void (*cb_t)(Args...);
+
+  public:
+    void listen(cb_t cb) {
+      callbacks.push_back(cb);
+    }
+
+    void unlisten(cb_t cb) {
+      callbacks.remove(cb);
+    }
+
+    void dispatch(Args... data) const {
+      for (cb_t cb : callbacks)
+        (*cb)(data...);
+    }
+
+  private:
+    list<cb_t> callbacks;
+};
+
+void many_args(int x, int y) {
+  cout << "many_args: " << x << ", " << y << endl;
+}
+
+void to_listen(string x) {
+  cout << "to_listen: " << x << endl;
+}
+
+void to_unlisten(string x) {
+  cout << "to_unlisten: " << x << endl;
+}
 
 void aldente() {
-  namespace asio = boost::asio;
-  namespace pst = boost::posix_time;
-  namespace bsys = boost::system;
+  auto string_event = EventManager<string>();
 
-  asio::io_service io;
-  asio::io_service::strand s(io);
-  asio::deadline_timer t1(io, pst::seconds(1));
-  asio::deadline_timer t2(io, pst::seconds(1));
+  string_event.listen(&to_listen);
+  string_event.listen(&to_unlisten);
 
-  int count = 0;
-  std::function<void (const bsys::error_code &)> c1 = s.wrap(
-    [&](const bsys::error_code &) {
-      if (count < 5) {
-        ++count;
-        std::cout << "c1: Counted " << count << " times." << std::endl;
-        t1.expires_at(t1.expires_at() + pst::seconds(1));
-        t1.async_wait(c1);
-      }
-    });
-  std::function<void (const bsys::error_code &)> c2 = s.wrap(
-    [&](const bsys::error_code &) {
-      if (count < 5) {
-        ++count;
-        std::cout << "c2: Counted " << count << " times." << std::endl;
-        t2.expires_at(t2.expires_at() + pst::seconds(1));
-        t2.async_wait(c2);
-      }
-    });
-  t1.async_wait(c1);
-  t2.async_wait(c2);
+  string_event.dispatch("hello");
+  string_event.unlisten(&to_unlisten);
+  string_event.dispatch("to_unlisten should be dead");
 
-  boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
-  t.join();
-  std::cout << "Final count: " << count << "." << std::endl;
+  auto many_event = EventManager<int, int>();
+
+  many_event.listen(&many_args);
+  many_event.dispatch(1, 2);
+  many_event.unlisten(&many_args);
+  many_event.dispatch(3, 4);
 }
